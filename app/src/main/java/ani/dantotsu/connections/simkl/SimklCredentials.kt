99085747568b66097d2ae13e20e1acdd17fccf86
ram <ramlakhanregar1+github@gmial.com>
2025-10-26 09:32:59 +0530

    fix

---
+++ app/src/main/java/ani/dantotsu/connections/simkl/SimklCredentials.kt
@@ -0,0 +1,7 @@
+package ani.dantotsu.connections.simkl
+import ani.dantotsu.BuildConfig
+
+object SimklCredentials {
+     val CLIENT_ID = BuildConfig.SIMKL_CLIENT_ID
+     val CLIENT_SECRET = BuildConfig.SIMKL_CLIENT_SECRET
+}
